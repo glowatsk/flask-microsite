07230faa81d1f768c2f15bf8872149dd430873be
glowatsk <glowatsk@gmail.com>
2018-01-23 12:58:13 -0700

    Adding bootstrap, fixing html templates.

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,22 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 
 from config import Config
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- app/models.py
+++ app/models.py
@@ -52,7 +52,7 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-    def get_password_reset_token(self, expires_in=600):
+    def get_reset_password_token(self, expires_in=600):
         return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in},
                             app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
     
